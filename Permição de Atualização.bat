4483020f8606df6f4c6bdb1fdc62e6490f6a4add
Lima08 <andersonlimabafsa@gmail.com>
2025-07-15 02:04:49 -0300

    feat: Adiciona nova funcionalidade e otimiza DB

---
+++ Permição de Atualização.bat
@@ -0,0 +1,7 @@
+@echo off
+cd /d "C:\Users\Lima\Downloads\Programação"
+git add .
+git commit -m "feat: Adiciona nova funcionalidade e otimiza DB"
+git push
+echo Comandos Git executados com sucesso!
+pause
